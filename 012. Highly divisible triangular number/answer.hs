fba95bf2477dcc81619301a52e062ce2a34e1eea
Robert Worman <tr3ppy@gmail.com>
2020-04-01 14:00:35 +1000

    Fix confusing name

--- 012. Highly divisible triangular number/answer.hs
+++ 012. Highly divisible triangular number/answer.hs
@@ -1,6 +1,6 @@
 factors :: Int -> Int
-factors n = head . dropWhile ((< n) . factors) . scanl1 (+) $ [1..] where
+factors n = head . dropWhile ((< n) . f) . scanl1 (+) $ [1..] where
   mx = round . sqrt . fromIntegral
-  factors y = (*2) . length $ filter ((== 0) . (rem y)) [1..(mx y)]
+  f y = (*2) . length $ filter ((== 0) . (rem y)) [1..(mx y)]
 
 answer = factors 500
