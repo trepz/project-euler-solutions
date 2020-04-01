688f47caa305da581b2a608b41784521eed196ef
Robert Worman <tr3ppy@gmail.com>
2020-04-01 13:34:48 +1000

    Condense problem 12 a bit

--- 012. Highly divisible triangular number/answer.hs
+++ 012. Highly divisible triangular number/answer.hs
@@ -1,9 +1,6 @@
 factors :: Int -> Int
-factors y = (*2) . length $ filter (\z -> rem y z == 0) [1..(mx y)] where
+factors n = head . dropWhile (\x -> (factors x) < n) . scanl1 (+) $ [1..] where
   mx = round . sqrt . fromIntegral
+  factors y = (*2) . length $ filter (\z -> rem y z == 0) [1..(mx y)]
 
-divs :: Int -> Int
-divs n = head . dropWhile (\x -> (factors x) < n) . scanl1 (+) $ [1..]
-
-answer :: Int
-answer = divs 500
+answer = factors 500
