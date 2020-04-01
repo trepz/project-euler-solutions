a8b48943a1161501c950bae439b8561aab860d55
Robert Worman <tr3ppy@gmail.com>
2020-04-01 13:55:29 +1000

    Use composition over lambda

--- 012. Highly divisible triangular number/answer.hs
+++ 012. Highly divisible triangular number/answer.hs
@@ -1,6 +1,6 @@
 factors :: Int -> Int
-factors n = head . dropWhile (\x -> (factors x) < n) . scanl1 (+) $ [1..] where
+factors n = head . dropWhile ((< n) . factors) . scanl1 (+) $ [1..] where
   mx = round . sqrt . fromIntegral
-  factors y = (*2) . length $ filter (\z -> rem y z == 0) [1..(mx y)]
+  factors y = (*2) . length $ filter ((== 0) . (rem y)) [1..(mx y)]
 
 answer = factors 500
