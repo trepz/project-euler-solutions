40fc759533f79dbfdcfcad67796a7269433efaff
Robert Worman <tr3ppy@gmail.com>
2020-04-05 16:20:45 +1000

    Cleaner approach to 67

--- 067. Maximum path sum II/answer.hs
+++ 067. Maximum path sum II/answer.hs
@@ -1,19 +1,16 @@
 import System.IO
 
 triangleToDigits :: String -> [[Int]]
-triangleToDigits = (map read <$>) . map words . lines
+triangleToDigits = reverse . (map read <$>) . map words . lines
 
 maxPathSum :: [[Int]] -> [Int]
 maxPathSum [x] = x
-maxPathSum (x:xs) = 
-  imap (\i v -> v + (maxNext i)) 0 x where 
-    t = maxPathSum xs
-    maxNext index = max (t !! index) (t !! (index + 1))
+maxPathSum (y:x:xs) = 
+  let m = zipWith (+) (largestNeighbour y) x
+  in maxPathSum (m:xs)
 
--- map with index
-imap :: (Num c) => (c -> a -> b) -> c -> [a] -> [b]
-imap f _ [] = []
-imap f i [x] = [f i x]
-imap f i (x:xs) = (f i x) : imap f (i + 1) xs
+largestNeighbour :: [Int] -> [Int]
+largestNeighbour [x] = []
+largestNeighbour (y:x:xs) = (max x y) : (largestNeighbour (x:xs))
 
-answer = head .  maxPathSum . triangleToDigits <$> readFile "triangle.txt"
+answer = head . maxPathSum . triangleToDigits <$> readFile "triangle.txt"
