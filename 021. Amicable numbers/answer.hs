c33dac16bc2622cb7cdaff5bb897e5d180f5b0b0
Robert Worman <tr3ppy@gmail.com>
2020-04-11 11:37:01 +1000

    Clean up problem 21 a bit more

--- 021. Amicable numbers/answer.hs
+++ 021. Amicable numbers/answer.hs
@@ -3,23 +3,15 @@ import Data.List
 divisors :: Int -> [Int]
 divisors n = 1 : filter ((==0) . rem n) [2 .. n `div` 2]
 
-indexOf :: [a] -> Int -> Maybe a
-indexOf list index
-  | index > (length list) - 1 = Nothing
-  | otherwise = Just (list !! index)
-
 pairs :: [Int] -> [(Maybe Int, Maybe Int, Maybe Int)]
 pairs list = map pair list where
-  p n = indexOf list (n - 1)
+  p n = if ((n - 1) > (length list)) then Nothing else Just (list !! (n - 1))
   pair n = (Just n, p n, p n >>= p)
 
-isAmicable :: (Eq a) => (a, a, a) -> Bool
-isAmicable (a, b, c) = a == c && a /= b
-
-answer = 
-  sum .
-  nub . 
-  map (\(a, _, _) -> maybe 0 id a) . 
-  filter isAmicable . 
-  pairs . 
-  map (sum . divisors) $ [1..10000]
+answer = sum
+       . nub
+       . map (\(a, _, _) -> maybe 0 id a)
+       . filter (\(a, b, c) -> a == c && a /= b)
+       . pairs 
+       . map (sum . divisors) 
+       $ [1..10000]
